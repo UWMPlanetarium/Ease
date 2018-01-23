a7f8a8bc24fdcf4a52b5d5ae10331bac125f6108
Sam <gallaghersam95@gmail.com>
2018-01-23 14:34:15 -0600

    Worked

--- Development/gScript.js
+++ Development/gScript.js
@@ -1,6 +1,6 @@
-var spreadSheet = SpreadsheetApp.openById("1DBDIBHKDEnXinGfvwHC62Yusx94wQomOdOd9s_LOJL0");
-var sheet = spreadSheet.getSheetByName("Test");
+// Globals
 var database = FirebaseApp.getDatabaseByUrl("https://development-c2673.firebaseio.com/");
+var cal_id = "q3qhk29ni908mhhrrvrha1lfq0@group.calendar.google.com";
 
 function doGet(e) {
 	getData();
@@ -12,8 +12,44 @@ function getData() {
 }
 
 function apiController(params) {
-	sheet.getRange(1, 1).setValue(params.fnct);
-	database.updateData("/functions/", {
-		response: "Yes"
-	});
+	switch(params.fnct) {
+
+		case "createEvent":
+			createEvent(params.data);
+			break;
+
+		default:
+			database.updateData("/functions/", {
+				response: "No such function."
+			});
+			break;
+
+	}
+}
+
+// API FUNCTIONS
+
+// CALENDAR
+// Create Event
+function createEvent(json) { // Required cal_id, {group {groupName, groupType}, event_DATA {calEvent {calStart, calEnd, calID}}}
+
+ 	var object = JSON.parse(json);
+ 	var calendar = CalendarApp.getCalendarById(cal_id);
+ 	var event = calendar.createEvent(
+ 		object.group.groupName + " - " + object.group.groupType,
+ 		new Date(object.event_DATA.calEvent.calStart),
+ 		new Date(object.event_DATA.calEvent.calEnd)
+ 	);
+ 	var id = event.getId();
+ 	object.event_DATA.calEvent.eventID = id;
+ 	var res = {
+ 		response: true,
+ 		id: id,
+ 		event_DATA: object.event_DATA
+ 	};
+ 	var json = JSON.stringify(res);
+ 	database.updateData("/functions/", {
+ 		response: json
+ 	});
+
 }
--- Development/scripts.js
+++ Development/scripts.js
@@ -2,9 +2,25 @@ function main() {
 
 	app.iframe = new app.Iframe();
 
-	app.iframe.request("generateInvoice", "Test").then(function(response) {
+	var data = {
+		group: {
+			groupName: "Sam",
+			groupType: "School"
+		},
+		event_DATA: {
+			calEvent: {
+				calStart: "Tue Jan 23 2018 18:30:00 GMT-0600 (CST)",
+				calEnd: "Tue Jan 23 2018 19:30:00 GMT-0600 (CST)",
+				eventID: false
+			}
+		}
+	};
+	var json = JSON.stringify(data);
 
-		console.log("It worked! The response is: " + response);
+	app.iframe.request("createEvent", json).then(function(response) {
+
+		console.log("It worked! The response is:");
+		console.log(response);
 
 	}, function(error) {
 
@@ -13,7 +29,6 @@ function main() {
 	});
 
 }
-
 
 var app = {};
 
@@ -34,84 +49,22 @@ app.Iframe = Backbone.Firebase.Model.extend({
 			});
 			$("#iframe").attr("src", "https://script.google.com/macros/s/AKfycbykE61pc7soyWhtW76U9HmvTJ218LPW_flmxCGM7mJi5LYXhr01/exec");
 
-			// Clean up
-			setTimeout(function() {
+			// Set a stopwatch for 8 seconds, if the response attribute doesn't change, reject the promise
+			proxy.listenToOnce(proxy, "change", function() {
 				$("#iframe").attr("src", "#");
+				resolve(proxy.attributes.response);
 			});
 
-			// Set a stopwatch for 8 seconds, if the response attribute doesn't change, reject the promise
-			var end = new Date().getTime() + 8000;
-			while (end > new Date().getTime()) {
-
-				if (proxy.attributes.response !== false) { // Response has changed
-					console.log(proxy.attributes.response);
-					resolve(proxy.attributes.response);
-				}
-
-			}
-
-			reject(Error("No response from server."));
-
-		});
-
-	}
-});
-
-main();
-
-
-/*
-		this.callback = callback;
-		this.set({
-			fnct: fnct,
-			data: data
-		});
-		$("#iframe").attr("src", "https://script.google.com/macros/s/AKfycbykE61pc7soyWhtW76U9HmvTJ218LPW_flmxCGM7mJi5LYXhr01/exec");
-		setTimeout(function() {
-			$("#iframe").attr("src", "#");
-		}, 50);
-
-
-function main() {
-	//$("#iframe").attr("src", "https://script.google.com/macros/s/AKfycbykE61pc7soyWhtW76U9HmvTJ218LPW_flmxCGM7mJi5LYXhr01/exec");
-	app.dataModel = new app.DataModel();
-	setTimeout(function() {
-		app.dataModel.set({
-			fnct: "sendEmail",
-			data: false
-		});
-	}, 500);
-}
-
-var app = {};
-// Backbone Components
-var flag = false;
-
-// Models
-app.DataModel = Backbone.Firebase.Model.extend({
-	urlRoot: "https://development-c2673.firebaseio.com/functions/",
-	autoSync: true,
-	initialize: function() {
-		this.on("change:fnct", this.sendRequest);
-		this.on("change:data", this.sendRequest);
-		this.on("change:response", this.hearResponse);
-	},
-	sendRequest: function() {
-		if (flag === false) {
-			flag = true;
-			$("#iframe").attr("src", "https://script.google.com/macros/s/AKfycbykE61pc7soyWhtW76U9HmvTJ218LPW_flmxCGM7mJi5LYXhr01/exec");
-			var proxy = this;
 			setTimeout(function() {
-				$("#iframe").attr("src", "#");
-				flag = false;
-			}, 2000);
-		}
-	},
-	hearResponse: function() {
-		console.log(this.attributes.response);
+				if (proxy.attributes.response === false) {
+					$("#iframe").attr("src", "#");
+					reject(Error("No response from server."));
+				}
+			}, 8000);
+
+		});
+
 	}
 });
 
-main();
-
-*/
+$(document).ready(main);
